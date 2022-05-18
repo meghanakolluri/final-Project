d245c9aba43de153b2402bbb93664023d1604ffe
meghanakolluri <kollurimeghana388@gmail.com>
2022-05-18 10:51:59 +0530

    Some more UI changes

--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -44,7 +44,7 @@ const UserCard = ({children, user, border, handleClose, setShowFollowers, setSho
     return (
         <div className={`d-flex p-2 align-items-center justify-content-between w-100 ${border}`}>
             <div>
-                <Link to={`/profile/${user._id}`} onClick={handleCloseAll}
+                <Link style={{color:"black"}}to={`/profile/${user._id}`} onClick={handleCloseAll}
                 className="d-flex align-items-center">
                     
                     <Avatar src={user.avatar} size="big-avatar" />
